a08af48884a8304cdaa6d200c6914c7dbf05cb61
tildetilde <tilde.egebrand@gmail.com>
2025-02-21 09:46:09 +0100

    Small fixes regarding semantics in HTML, dark mode

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger-menu");
-  const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
 
-  hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-    hamburger.classList.toggle("open");
-  });
+hamburger.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("open");
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const darkModeToggle = document.getElementById("dark-mode-toggle");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-  darkModeToggle.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-  });
+darkModeToggle.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
 });
